greatwolf/KKClient

commit 6f9c4d27c7700e663f7b37f3ebcff9b87e1ab14b

Refactored 'GetReceiveAddress' and 'GetPublicKey' in background.js. The old approach made two calls to device: first call to get address from device and second call to display it on keepkey.
The new approach just uses the addresses and xpub KKClient already knows
about and only sends one call to keepkey to display it. This eliminates
an extra unnecessary call and makes verifying address/xpub on device
easier to implement later on.

Updated 'ReceiveController' and 'ViewXPubController' in popup.js to
handle this new approach. Also have 'PinLockService' use
'ButtonRequest_Address' instead of 'Address' message to handle
transitioning out of 'verifying' state.

Background.js
@@ -2238,25 +2238,17 @@ var _typeof2 = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator
         return e.prototype.action = function(e)
           {
             var t = a.AccountListManager.findAccount(e.account),
-              n = t.coinType === o.CoinName.Ethereum,
-              i;
-            return t.getReceiveAddressDetails(e.depth).then(function(e)
+              n = t.coinType === o.CoinName.Ethereum;
+            return t.getReceiveAddressDetails(e.depth).then(function(i)
             {
-              return i = e,
-                n ? t.deviceClient.getEthereumAddress(i.nodePath, !1) : t.deviceClient.getAddress(i.nodePath, i.coinType, !1)
-            }).then(function(o)
-            {
-              if (!o) return;
-              n ? t.deviceClient.getEthereumAddress(i.nodePath, !0) : t.deviceClient.getAddress(i.nodePath, i.coinType, !0);
-              var s;
-              return s = o.address_str ? o.address_str : n && o.address ? "0x" + o.address.toHex() : o.address,
-                r.UiMessenger.sendMessageToUI("ReceiveAddress",
-                {
-                  account: e.account,
-                  depth: e.depth,
-                  address: s,
-                  nodePath: i.nodePath.toString()
-                })
+              r.UiMessenger.sendMessageToUI("ReceiveAddress",
+              {
+                account: e.account,
+                depth: e.depth,
+                address: i.address,
+                nodePath: i.nodePath.toString()
+              })
+              return n ? t.deviceClient.getEthereumAddress(i.nodePath, !0) : t.deviceClient.getAddress(i.nodePath, i.coinType, !0);
             })
           },
           e
@@ -7134,7 +7126,7 @@ var _typeof2 = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator
           return this.sendMessageToUi(e.$type.name, e),
             null
         },
-        e.messageNames = ["Address", "ButtonRequest", "CharacterRequest", "Failure", "PinMatrixRequest", "Success", "PassphraseRequest", "WordRequest"],
+        e.messageNames = ["Address", "EthereumAddress", "ButtonRequest", "CharacterRequest", "Failure", "PinMatrixRequest", "Success", "PassphraseRequest", "WordRequest"],
         e
     }();
     n.MessageForwarder = r
@@ -80185,24 +80177,28 @@ var _typeof2 = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator
         }
         return e.prototype.action = function(e)
           {
-            var t = a.AccountListManager.findAccount(e.walletId),
-              nodePath;
+            var t = a.AccountListManager.findAccount(e.walletId);
             var coinName = t.wallet.api.apiHelper.coinType.name;
-            return t.nodePathPromise.then(function(e)
-            {
-              return nodePath = e,
-                t.deviceClient.getPublicKey(nodePath, coinName, !1)
-            }).then(function(o)
-            {
-              if (!o) return;
-              t.deviceClient.getPublicKey(nodePath, coinName, !0);
-              return r.UiMessenger.sendMessageToUI("PublicKey",
-                {
-                  account: e.walletId,
-                  address: o.xpub,
-                  nodePath: nodePath.toString()
-                })
-            })
+            return t.nodePathPromise
+                    .then(function(nodePath)
+                    {
+                      r.UiMessenger.sendMessageToUI("PublicKey",
+                      {
+                        account: e.walletId,
+                        address: t.wallet.data.xpub,
+                        nodePath: nodePath.toString()
+                      })
+                      return t.deviceClient.getPublicKey(nodePath, coinName, !0)
+                    })
+                    .then(function(o)
+                    {
+                      if (!o) return;
+                      return r.UiMessenger.sendMessageToUI("XPubAddress",
+                      {
+                        address: o.xpub,
+                        typename: "XPubAddress"
+                      })
+                    })
           },
           e
       }();

chrome-wallet/popup.js
@@ -1671,11 +1671,11 @@ angular.module('kkWallet', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'monospaced.
   {
     c.setNextTransition('slideLeft'),
       new ClipboardJS('.copy-to-clipboard-button');
-    var d = new Promise(function(t)
+    var d = new Promise(function(resolve)
       {
-        e.$on('ButtonRequest', function(e, n)
+        t.$on('ButtonRequest', function(e, n)
         {
-          'ButtonRequest_Address' === n.code && t(n.code)
+          'ButtonRequest_Address' === n.code && (resolve(n.code), t.busy = false)
         })
       }),
       u = !1;
@@ -1684,6 +1684,7 @@ angular.module('kkWallet', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'monospaced.
       t.wallet = i.getWalletById(t.walletId),
       t.currency = t.wallet.coinType,
       t.maxDepth = r.maxReceiveAddresses - 1,
+      t.busy = true,
       t.isSingleAddressAccount = 'single' === t.wallet.addressStrategy
       switch(pinLock.state)
       {
@@ -1955,15 +1956,16 @@ angular.module('kkWallet', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'monospaced.
   {
     c.setNextTransition('slideLeft'),
       new ClipboardJS('.copy-to-clipboard-button');
-    var d = new Promise(function(t)
+    var d = new Promise(function(resolve)
       {
-        e.$on('ButtonRequest', function(e, n)
+        t.$on('ButtonRequest', function(e, n)
         {
-          'ButtonRequest_Address' === n.code && t(n.code)
+          'ButtonRequest_Address' === n.code && (resolve(n.code), t.busy = false)
         })
       }),
       u = !1;
-      t.currency = i.getWalletById(n.walletId).coinType
+      t.currency = i.getWalletById(n.walletId).coinType,
+      t.busy = true
       switch(pinLock.state)
       {
         case pinLock.idling:
@@ -2048,6 +2050,7 @@ angular.module('kkWallet', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'monospaced.
             this.request.message.state = n[1],
             e.invoke(t('/buttonRequest/:code/:policy/:state'), this)) : e.invoke(t('/buttonRequest/:code'), this)
         }
+        if ('ButtonRequest_Address' === this.request.message.code) pinLock.reset()
       }]),
       e.when('WordRequest', t('/wordRequest')),
       e.when('CharacterRequest', t('/characterRequest/:word_pos/:character_pos')),
@@ -2226,11 +2229,9 @@ angular.module('kkWallet', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'monospaced.
         t.set(this.request.message),
           e.go('/confirm-exchange')
       }]),
-      e.when('Address', ['PinLockService', function(pinLock)
-      {
-        if (pinLock.state === pinLock.verifying)
-          pinLock.reset();
-      }]),
+      e.when('Address', function() {}),
+      e.when('EthereumAddress', function() {}),
+      e.when('XPubAddress', function() {}),
       e.when('ReceiveAddress', ['ReceiveAddressService', function(e)
       {
         e.set(this.request.message)
@@ -3315,12 +3316,12 @@ angular.module('kkWallet', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'monospaced.
       e.put('app/popup/pin-timeout/pin-timeout.tpl.html', '<section id=label ng-controller=PinTimeoutController><header><back-button class="button-left header-button"></back-button></header><div class=content><h2><span class=cornflower-txt><b>{{\'Change Pin Timeout\'}}</b></span> For Your<vendor-name></vendor-name></h2><form name=form novalidate><div class=form-input-container><div class=content-form-inner><div class=content-inner-centering><div class=help>Change how long your device will wait before it requires you to enter your pin again.</div><label>Pin Timeout (in minutes)</label><div class=invalid ng-show=form.$submitted><div ng-show=form.deviceLabel.$error.required>Required</div></div><form name=form><input name=input type=number autocomplete=off ng-model=timeout required min=2 max=120 focus><div role=alert><span class=form-error ng-show=form.input.$error.required>Required!</span> <span class=form-error ng-show=form.input.$error.number>Not valid number!</span> <span class=form-error ng-show=form.input.$error.min>Below minimum!</span> <span class=form-error ng-show=form.input.$error.max>Above maximum!</span></div></form></div></div></div><button class=button type=submit ng-click=nextAction()>Set Pin Timeout</button></form><notification></notification></div></section>'),
       e.put('app/popup/preparing/preparing.tpl.html', '<section id=preparing ng-controller=PreparingController><header><back-button class="button-left header-button"></back-button></header><div class=content><div class=content-no-button-inner><div class=content-inner-centering><h2><span class=cornflower-txt><b>Preparing Transaction.</b></span><br>Please wait while your<br>transaction is prepared...</h2><div id=loader><div></div><div></div><div></div><div></div><div></div><div class=second-top></div><div class=top></div></div><div class=progress-indicator ng-show=progress.total>{{ progress.current }} of {{ progress.total }}</div></div></div></div></section>'),
       e.put('app/popup/prerelease-device/prerelease-device.tpl.html', '<section id=prerelease-device ng-controller=SupportController><div class=content><h2><p style="font-size: 18px">Device Error</p><p>An error was detected on your device. Please <a target=_blank href=https://support.keepkey.com/support/tickets/new>contact support</a>. (Code: KH-00)</p></h2><div class=menu><ul><li><button class=menu-item ng-click=launchSupportTab()>Web</button></li><li><button class=menu-item ng-click=openEmail()>Email {{supportEmailAddress}}</button></li></ul></div><div class=menu-annotation>Telephone: {{supportPhoneNumber}}</div></div></section>'),
-      e.put('app/popup/receive/receive.tpl.html', '<section id=receive ng-controller=ReceiveController><header><back-button class="button-left header-button"></back-button></header><div class=content><h2><span class=cornflower-txt><b>Receive</b></span> {{currency}}</h2><div class=content-inner><div class=content-inner-centering><div class=help>Confirm the address below is the same one shown on your<vendor-name></vendor-name>before using it.</div><qrcode ng-show=unusedAddress.address class=qrcode data={{unusedAddress.address}} size=160 error-correction-level=L target=_blank></qrcode><div ng-show=unusedAddress.address><span class=bitcoin-receive-link>{{unusedAddress.address.replace("bitcoincash:","")}}</span> <button class="copy-to-clipboard-button fa fa-clipboard" data-clipboard-text={{unusedAddress.address}}></button><notification></notification></div><div class=node-path>{{unusedAddress.nodePath}}</div></div><div class="address-nav prev fa fa-chevron-left" ng-hide=isSingleAddressAccount ng-class="{disabled: addressDepth <= 0}" ng-click="showAnotherAddress(addressDepth - 1)"></div><div class="address-nav next fa fa-chevron-right" ng-hide=isSingleAddressAccount ng-class="{disabled: addressDepth >= maxDepth}" ng-click="showAnotherAddress(addressDepth + 1)"></div></div></div><div class="content in-progress-overlay" ng-hide=unusedAddress.address><div class=content-no-button-inner><div class=content-inner-centering><h2><span class=cornflower-txt><b>Finding unused address.</b></span><br>Please wait while we<br>find an unused address...</h2><div id=loader><div></div><div></div><div></div><div></div><div></div><div class=second-top></div><div class=top></div></div></div></div></div></section>'),
+      e.put('app/popup/receive/receive.tpl.html', '<section id=receive ng-controller=ReceiveController><header><back-button class="button-left header-button"></back-button></header><div class=content><h2><span class=cornflower-txt><b>Receive</b></span> {{currency}}</h2><div class=content-inner><div class=content-inner-centering><div class=help>Confirm the address below is the same one shown on your<vendor-name></vendor-name>before using it.</div><qrcode ng-show=unusedAddress.address class=qrcode data={{unusedAddress.address}} size=160 error-correction-level=L target=_blank></qrcode><div ng-show=unusedAddress.address><span class=bitcoin-receive-link>{{unusedAddress.address.replace("bitcoincash:","")}}</span> <button class="copy-to-clipboard-button fa fa-clipboard" data-clipboard-text={{unusedAddress.address}}></button><notification></notification></div><div class=node-path>{{unusedAddress.nodePath}}</div></div><div class="address-nav prev fa fa-chevron-left" ng-hide=isSingleAddressAccount ng-class="{disabled: addressDepth <= 0}" ng-click="showAnotherAddress(addressDepth - 1)"></div><div class="address-nav next fa fa-chevron-right" ng-hide=isSingleAddressAccount ng-class="{disabled: addressDepth >= maxDepth}" ng-click="showAnotherAddress(addressDepth + 1)"></div></div></div><div class="content in-progress-overlay" ng-hide=!busy><div class=content-no-button-inner><div class=content-inner-centering><h2><span class=cornflower-txt><b>Finding unused address.</b></span><br>Please wait while we<br>find an unused address...</h2><div id=loader><div></div><div></div><div></div><div></div><div></div><div class=second-top></div><div class=top></div></div></div></div></div></section>'),
       e.put('app/popup/send/preparing.tpl.html', '<div class="content in-progress-overlay"><div class=content-no-button-inner><div class=content-inner-centering><h2><span class=cornflower-txt><b>Preparing Transaction.</b></span><br>Please wait while your<br>transaction is prepared...</h2><div id=loader><div></div><div></div><div></div><div></div><div></div><div class=second-top></div><div class=top></div></div><div class=progress-indicator ng-show=progress.total>{{ progress.current }} of {{ progress.total }}</div></div></div></div>'),
       e.put('app/popup/send/send.tpl.html', '<section id=build-transaction ng-controller=SendController><header><back-button class="button-left header-button"></back-button></header><div class=content><div ng-show=showForm><account-balance account=wallet single-account=singleAccount loading=false name-display=name currency=currency fresh=fresh></account-balance><form name=form novalidate ng-class="{exchange: isExchange}"><div class=form-input-container><div class="content-form-inner sending"><div class=content-header><recipient-entry recipient=userInput.address field-name=dest form=form currency-name={{currency}} current-account=wallet.id disabled=preparingTransaction filter-recipient-for-exchange=false></recipient-entry></div><div class=exchange-images ng-class="{\'image-centered\': !userInput.address}"><div><img class=currency-logo ng-src={{accountCurrencyUrl}}><div class=centered>{{currencySymbol}}</div></div><i class="fa fa-arrow-right" ng-if="isExchange || userInput.address"></i><div class=shapeshift-exchange-container ng-if=isExchange><div class=shapeshift-fox-container><img class=fox src=assets/partner-logos/shapeshift.svg></div></div><i class="fa fa-arrow-right" ng-if=isExchange></i><div ng-if=destinationCurrency><img class=currency-logo ng-src={{destinationCurrencyUrl}}><div class=centered>{{destinationCurrencySymbol}}</div></div><div ng-if="userInput.address && !destinationCurrency"><img class=currency-logo ng-src={{accountCurrencyUrl}}><div class=centered>external address</div></div></div><div class="content-inner-centering sending" ng-class="{\'two-column\': isExchange}"><div class=amount-container><amount-entry amount=userInput.amount max-amount=maxSendAmount.toString() min-amount=minSendAmount.toString() max-reason=maxReason min-reason=minReason field-name=amount form=form disable=preparingTransaction currency=currency is-exchange-amount-label=false loading=!fresh.status></amount-entry><fee-selector ng-show="config.showFeeSelector && !isExchange && feeLevels.length > 1" fee-levels=feeLevels estimated-fees=estimatedFee.fee selected=userInput.feeLevel active="!preparingTransaction && userInput.amount > 0"></fee-selector></div><div class=value-container ng-show=isExchange><div class=value-display><label>Estimated receive amount:</label><div class="estimated-value value"><formatted-amount ng-show=estimatedReceiveAmount amount=estimatedReceiveAmount currency=destinationCurrency></formatted-amount>&nbsp;</div></div></div></div></div></div><div class=fee-container ng-class="{\'two-column\': isExchange}"><div class=value-container><div class="fee-display value-display"><label>Miner fee:</label><div class=value><formatted-amount class=fee ng-show=showFee() amount=getFee(userInput.feeLevel) currency=getFeeCurrency()></formatted-amount></div></div></div><div class=value-container ng-show=isExchange><div class="exchange-rate-display value-display"><label>Estimated rate:</label><div class=value><span>1&nbsp;{{currencySymbol}}&nbsp;&asymp;&nbsp;</span><formatted-amount amount=exchangeMarketInfo.rate currency=destinationCurrency></formatted-amount></div></div></div></div><div class="alert-info sending-alert" ng-show=exchangeDenied()><span ng-if="exchangeStatus.errorCode === \'geoRestriction\'">ShapeShift is not available in your area. Please see <a href={{exchangeStatus.url}} target=_blank>{{exchangeStatus.url}}</a> for more information.</span></div><button class=button type=submit ng-show="!showLogin || !isExchange" ng-disabled="preparingTransaction || !enableExchangeButton" ng-click=buildTransaction()>{{buttonText}}</button><div ng-show="showLogin && isExchange" class=login-prompt><span>You must log in to your ShapeShift Account in order to trade assets.</span> <button class=button ng-click=loginToSS()>Log in to ShapeShift</button></div></form><notification></notification></div><div ng-hide=showForm>Can\'t send from an account with a high confidence balance of 0</div></div><ng-include src="\'app/popup/send/preparing.tpl.html\'" ng-if=preparingTransaction></ng-include></section>'),
       e.put('app/popup/sending/sending.tpl.html', '<section id=sending><header><back-button class="button-left header-button" action=cancelDeviceOperation();></back-button></header><div class=content><h2><span class=cornflower-txt><b>Signing Transaction</b></span><br>follow the prompts on your<vendor-name after=...></vendor-name></h2><div id=loader><div></div><div></div><div></div><div></div><div></div><div class=second-top></div><div class=top></div></div><a class=button ng-click=cancelDeviceOperation();>Cancel</a><notification></notification></div><footer ng-include="\'app/popup/footer/footer.tpl.html\'"></footer></section>'),
       e.put('app/popup/success/buildingTransaction.tpl.html', '<section id=success ng-controller=SuccessController><header></header><div class=content><div class=content-no-button-inner><div class=content-inner-centering><h2><span class=cornflower-txt><b>Signing transaction.</b></span><br>Please wait while your<vendor-name></vendor-name>signs the transaction.</h2><div id=loader><div></div><div></div><div></div><div></div><div></div><div class=second-top></div><div class=top></div><notification></notification></div></div></div></div></section>'),
-      e.put('app/popup/xpub/xpub.tpl.html', '<section id=xpub ng-controller=ViewXPubController><header><back-button class="button-left header-button"></back-button></header><div class=content><h2>{{currency}} <span class=cornflower-txt><b>XPub Address</b></span></h2><div class=content-inner><div class=content-inner-centering><div class=help>Confirm the address below is the same one shown on your<vendor-name></vendor-name>before using it.</div><qrcode ng-show=unusedAddress.address class=qrcode data={{unusedAddress.address}} size=160 version=7 error-correction-level=M target=_blank></qrcode><div class=address ng-show=unusedAddress.address><span class=bitcoin-receive-link>{{unusedAddress.address}}</span> <button class="copy-to-clipboard-button fa fa-clipboard" data-clipboard-text={{unusedAddress.address}}></button><notification></notification></div><div class=node-path>{{unusedAddress.nodePath}}</div></div></div></div><div class="content in-progress-overlay" ng-hide=unusedAddress.address><div class=content-no-button-inner><div class=content-inner-centering><h2><span class=cornflower-txt><b>Finding unused address.</b></span><br>Please wait while we<br>find an unused address...</h2><div id=loader><div></div><div></div><div></div><div></div><div></div><div class=second-top></div><div class=top></div></div></div></div></div></section>'),
+      e.put('app/popup/xpub/xpub.tpl.html', '<section id=xpub ng-controller=ViewXPubController><header><back-button class="button-left header-button"></back-button></header><div class=content><h2>{{currency}} <span class=cornflower-txt><b>XPub Address</b></span></h2><div class=content-inner><div class=content-inner-centering><div class=help>Confirm the address below is the same one shown on your<vendor-name></vendor-name>before using it.</div><qrcode ng-show=unusedAddress.address class=qrcode data={{unusedAddress.address}} size=160 version=7 error-correction-level=M target=_blank></qrcode><div class=address ng-show=unusedAddress.address><span class=bitcoin-receive-link>{{unusedAddress.address}}</span> <button class="copy-to-clipboard-button fa fa-clipboard" data-clipboard-text={{unusedAddress.address}}></button><notification></notification></div><div class=node-path>{{unusedAddress.nodePath}}</div></div></div></div><div class="content in-progress-overlay" ng-hide=!busy><div class=content-no-button-inner><div class=content-inner-centering><h2><span class=cornflower-txt><b>Finding unused address.</b></span><br>Please wait while we<br>find an unused address...</h2><div id=loader><div></div><div></div><div></div><div></div><div></div><div class=second-top></div><div class=top></div></div></div></div></div></section>'),
       e.put('app/popup/success/success.tpl.html', '<section id=success ng-controller=SuccessController><header></header><div class="content notification"><div class=content-inner-with-button><div class=description><div class=alert-icon><span class="fa fa-check-square"></span></div><div><p>{{message}}</p><notification></notification></div></div></div><a class=button ng-click=ok()>OK</a></div><footer ng-include="\'app/popup/footer/footer.tpl.html\'"></footer></section>'),
       e.put('app/popup/success/updatingFirmware.tpl.html', '<section id=success ng-controller=SuccessController><div class=content><div class=content-no-button-inner><div class=content-inner-centering><h2><span class=cornflower-txt><b>Updating firmware.</b></span><br>Please wait while your<vendor-name after="\'s"></vendor-name>firmware is updated. Do not unplug the device.</h2><div id=loader><div></div><div></div><div></div><div></div><div></div><div class=second-top></div><div class=top></div><notification></notification></div></div></div></div></section>'),
       e.put('app/popup/success/uploadComplete.tpl.html', '<section id=success ng-controller=SuccessController><header></header><div class="content success-upload-complete"><div class=content-no-button-inner><div class=content-inner-centering><h2><span class=cornflower-txt><b>Firmware update complete.</b></span><br>Please disconnect and reconnect your device, as well as close and reopen the KeepKey Client.</h2><div class=success></div><notification></notification></div></div></div><footer ng-include="\'app/popup/footer/footer.tpl.html\'"></footer></section>'),

commit fdf8f410069f61fb17936f0c79c4651313aa254f

Proxy 'Background.js' no longer sends stray 'EthereumAddress' messages to UI 'popup.js' during startup or refresh. Problem was introduced from the refactor in 6f9c4d2.

Background.js
@@ -2269,7 +2269,10 @@ var _typeof2 = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator
                 address: i.address,
                 nodePath: i.nodePath.toString()
               })
-              return n ? t.deviceClient.getEthereumAddress(i.nodePath, !0) : t.deviceClient.getAddress(i.nodePath, i.coinType, !0);
+              return n ? t.deviceClient.getEthereumAddress(i.nodePath, !0).then(function(e)
+              {
+                r.UiMessenger.sendMessageToUI(e.$type.name, e)
+              }) : t.deviceClient.getAddress(i.nodePath, i.coinType, !0);
             })
           },
           e
@@ -7206,7 +7209,7 @@ var _typeof2 = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator
           return this.sendMessageToUi(e.$type.name, e),
             null
         },
-        e.messageNames = ["Address", "EthereumAddress", "ButtonRequest", "CharacterRequest", "Failure", "PinMatrixRequest", "Success", "PassphraseRequest", "WordRequest"],
+        e.messageNames = ["Address", "ButtonRequest", "CharacterRequest", "Failure", "PinMatrixRequest", "Success", "PassphraseRequest", "WordRequest"],
         e
     }();
     n.MessageForwarder = r