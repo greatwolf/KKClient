779f1cd73aefefb7b8ad9728dd3c0d4d628330d7
greatwolf <github.greatwolf@mamber.net>
2022-12-01 22:33:33 -0800

    Bumped version to 6.7.6.

--- Background.js
+++ Background.js
@@ -12178,7 +12178,7 @@ var _typeof2 = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator
   {
     t.exports = {
       name: "KeepKey Client",
-      version: "6.7.5",
+      version: "6.7.6",
       description: "A simple cryptocurrency wallet that integrates with the KeepKey device",
       manifest_version: 2,
       icons:
